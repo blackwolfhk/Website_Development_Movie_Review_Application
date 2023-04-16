4b71cee9f0ad322251ba45deb666c8cf4aeb2dcb
kelvincheung000 <cheungkamkit@gmail.com>
2023-04-17 02:12:51 +0800

    Added get all movies and get movie APIs

--- backend/src/main/java/dev/blackwolf/movies/MoviesApplication.java
+++ backend/src/main/java/dev/blackwolf/movies/MoviesApplication.java
@@ -2,15 +2,14 @@ package dev.blackwolf.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class MoviesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
-
 
     }
 
